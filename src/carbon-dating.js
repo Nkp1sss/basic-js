bb1b3640e009dd73e13073277915d96a0f76b57b
Nikita <nikitakemper555@gmail.com>
2022-10-12 18:00:59 +0300

    feat: solve an assignment 'carbon dating'

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,7 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
+const MODERN_ACTIVITY = 15; // современная активность изотопа C14
+const HALF_LIFE_PERIOD = 5730; // период полураспада
 
 /**
  * Determine the age of archeological find by using
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity != 'string' || typeof sampleActivity == 'undefined' || +sampleActivity >= 15) 
+    return false;
+
+  let parse = parseInt(sampleActivity);
+  if (isNaN(parse)) return false;
+
+  if (+sampleActivity <= 0) return false;
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil( (Math.log(MODERN_ACTIVITY / sampleActivity)) / k );
 }
 
 module.exports = {
