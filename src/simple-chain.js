9398bb43f969da21f61a51c0177d1aa6f30c752f
Nikita <nikitakemper555@gmail.com>
2022-10-14 13:21:34 +0300

    feat: add solution 'simple-chain'

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,90 +4,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
-  chain: '',
-  arrayValues: [],
-  getLength() {
-    let length = 0;
-    let arraySymbols = this.chain.split('');
-    arraySymbols.forEach(symbol => {
-      if (symbol == '(')
-        length++;
-    });
 
-    return length
+const chainMaker = {
+  chain: [],
+
+  getLength() {
+    return this.chain.length;
   },
   addLink(value) {
-    if (this.chain == '')
-      this.chain += `( ${value} )~`;
-    else {
-      this.chain += `~( ${value} )~`;
-    }
-
-    this.arrayValues.push(value);
-
+    this.chain.push(value);
+    
     return this;
   },
   removeLink(position) {
-    let count = 1;
-    let arraySymbols = this.chain.split('');
-    let newChain = '';
+    if(Number.isInteger(position) && position <= this.chain.length && position > 0) {
+      this.chain.splice(position - 1, 1)
 
-    this.arrayValues.splice(position - 1, 1);
-
-    for (let i = 0; i < arraySymbols.length; i++) {
-      if (arraySymbols[i] == '(') {
-        if (count == position) {
-
-          // если надо удалить не первый элемент, то i уменьшаем на 2, чтобы удалить еще и ~~
-          if (i != 0)
-            newChain = newChain.slice(0, newChain.length - 2);
-
-          do {
-            i++;
-          }
-          while (arraySymbols[i] != ')');
-          if (arraySymbols[i] == ')' && position == 1) {
-            i += 2;
-          }
-          // if (this.getLength() == position)
-          //   newChain += '~';
-
-
-          position = -1;
-        }
-        else {
-          count++;
-          newChain += arraySymbols[i];
-        }
-      }
-      else {
-        newChain += arraySymbols[i];
-      }
+      return this;
+    }
+    else {
+      this.chain = [];
+      throw new Error("You can\'t remove incorrect link!")
     }
 
-    this.chain = newChain;
-    return this;
   },
   reverseChain() {
-    let reversed = this.arrayValues.reverse();
-
-    this.arrayValues = [];
-    this.chain = '';
-
-    reversed.forEach(element => {
-      this.addLink(element);
-    });
+    this.chain = this.chain.reverse();
 
     return this;
   },
   finishChain() {
-    return this.chain.slice(0, this.chain.length - 1);
+    let stringChain ='';
+
+    this.chain.forEach(element => {
+      stringChain += `( ${element} )~~`
+    });
+
+    this.chain = []; 
+    return stringChain.slice(0, stringChain.length - 2);
   }
 };
 
 module.exports = {
   chainMaker
 };
-
-console.log(chainMaker.finishChain());
