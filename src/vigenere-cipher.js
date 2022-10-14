20cae0934568d80e7130fa8cf0d6c6169d607157
Nikita <nikitakemper555@gmail.com>
2022-10-14 20:10:05 +0300

    feat: add solution 'encode-line'

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (arguments.length == 0) {
+      throw new Error('Incorrect arguments!')
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage, key) {
+    if (arguments.length == 0) {
+      throw new Error('Incorrect arguments!')
+    }
   }
 }
 
