7d549242deb781a129a8ad6c632a9ec395a87dc2
Nikita <nikitakemper555@gmail.com>
2022-10-15 18:54:10 +0300

    feat: add solution 'vigenere-cipher'

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,19 +20,124 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt(message, key) {
-    throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-    if (arguments.length == 0) {
-      throw new Error('Incorrect arguments!')
-    }
+  constructor(flag = true) {
+    this.flag = flag;
   }
-  decrypt(encryptedMessage, key) {
-    throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-    if (arguments.length == 0) {
-      throw new Error('Incorrect arguments!')
+
+  encrypt(message, key) {
+    if (!message || typeof message != 'string' || message.length == 0 || !key || typeof key != 'string'
+    || key.length == 0) {
+      throw new Error('Incorrect arguments!');
     }
+
+
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabetLength = alphabet.length;
+
+    let listValues = message.toLowerCase().split('').map(letter => {
+      if (alphabet.indexOf(letter) == -1) {
+        return letter;
+      }
+      return alphabet.indexOf(letter);
+    })
+
+
+    let threadKeys = [];
+    for (let i = 0; threadKeys.length < listValues.length;) {
+      for (let j = 0; j < key.length; j++) {
+        if (threadKeys.length == listValues.length) break;
+
+        if (typeof listValues[i] == 'number')
+          threadKeys.push(alphabet.indexOf(key[j].toLowerCase()));
+        else {
+          threadKeys.push(listValues[i]);
+          j--;
+        }
+        i++;
+      }
+    }
+    threadKeys = threadKeys.slice(0, listValues.length);
+
+
+    let encryptedValues = [];
+    for (let i = 0; i < threadKeys.length; i++) {
+      if (typeof listValues[i] == 'number')
+        encryptedValues.push((listValues[i] + threadKeys[i]) % alphabetLength);
+      else
+        encryptedValues.push(listValues[i]);
+    }
+
+
+    let encryptedText = encryptedValues.map(value => {
+      if (typeof value == 'number')
+        return alphabet[value];
+      else
+        return value;
+    })
+
+
+    if (this.flag)
+      return encryptedText.join('').toUpperCase();
+    else 
+      return encryptedText.reverse().join('').toUpperCase();
+  }
+
+  // тот же самый метод, за исключением того, что значения и ключи потока вычитаются по модулю 26, а не слаживатся.
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || typeof encryptedMessage != 'string' || encryptedMessage.length == 0 || !key || typeof key != 'string'
+      || key.length == 0) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabetLength = alphabet.length;
+
+    let listValues = encryptedMessage.toLowerCase().split('').map(letter => {
+      if (alphabet.indexOf(letter) == -1) {
+        return letter;
+      }
+      return alphabet.indexOf(letter);
+    })
+
+
+    let threadKeys = [];
+    for (let i = 0; threadKeys.length < listValues.length;) {
+      for (let j = 0; j < key.length; j++) {
+        if (threadKeys.length == listValues.length) break;
+
+        if (typeof listValues[i] == 'number')
+          threadKeys.push(alphabet.indexOf(key[j].toLowerCase()));
+        else {
+          threadKeys.push(listValues[i]);
+          j--;
+        }
+        i++;
+      }
+    }
+    threadKeys = threadKeys.slice(0, listValues.length);
+
+
+    let encryptedValues = [];
+    for (let i = 0; i < threadKeys.length; i++) {
+      if (typeof listValues[i] == 'number')
+        encryptedValues.push((listValues[i] - threadKeys[i] + alphabetLength) % alphabetLength);
+      else
+        encryptedValues.push(listValues[i]);
+    }
+
+
+    let encryptedText = encryptedValues.map(value => {
+      if (typeof value == 'number')
+        return alphabet[value];
+      else
+        return value;
+    })
+
+
+    if (this.flag)
+      return encryptedText.join('').toUpperCase();
+    else 
+      return encryptedText.reverse().join('').toUpperCase();
   }
 }
 
